ac6843a9a3683546ed79f1ec9ddd0bac00726c64
joerg <joerg@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-03-16 14:32:11 +0000

    copy_icons_to_debian_package_dirs.pl: add usage, change getting filename from geoinfo->name to condition->[k=poi]=xx
    git-svn-id: http://svn.openstreetmap.org/applications/share/map-icons@7091 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- copy_icons_to_debian_package_dirs.pl
+++ copy_icons_to_debian_package_dirs.pl
@@ -2,6 +2,7 @@
 # This little helper copies according to the icons.xml File
 # and the geoinfo->restrictions tag
 # all icons will be placed into there package directory
+# For more Inco see --man option
 
 use strict;
 use warnings;
@@ -10,6 +11,8 @@ use XML::Simple;
 use File::Basename;
 use File::Path;
 use File::Copy;
+use Getopt::Long;
+use Pod::Usage;
 
 my $file = 'icons';
 
@@ -31,14 +34,41 @@ my @theme_dirs=qw(classic.big classic.small
 		  svg svg_tn
 		  );
 
+my ($man,$help,$DEBUG,$VERBOSE)=(0,0,0,0);
+# Set defaults and get options from command line
+Getopt::Long::Configure('no_ignore_case');
+GetOptions ( 
+	     'd+'                  => \$DEBUG,
+	     'debug+'              => \$DEBUG,      
+	     'verbose'             => \$VERBOSE,
+	     'v+'                  => \$VERBOSE,
+	     'h|help|x'            => \$help, 
+	     )
+    or pod2usage(1);
+
+pod2usage(1) if $help;
+pod2usage(-verbose=>2) if $man;
+
+
 #-----------------------------------------------------------------------------
-my $rules = XMLin("$file.xml");
+my $rules = XMLin("$file.xml",
+		  ForceArray => [ 'condition' ],
+    );
 my @rules=@{$rules->{rule}};
 for my $rule (@rules) {
-    #print Dumper(\$rule);
+    print Dumper(\$rule) if $DEBUG >10;
     my $restricted = $rule->{'geoinfo'}->{'restricted'}||'';
-    my $name = $rule->{'geoinfo'}->{'name'};
-    $name =~s,\.,/,g;
+    my $names = $rule->{'condition'};
+    my ($name) = grep { $_->{k} =~ /^poi|rendering$/ } @{$names};
+    $name = $name->{v};
+    print "name: '$name'\n" if $VERBOSE;
+    if ( ! $name ) {
+	warn "Undefined Name\n";
+	warn Dumper(\$rule);
+	next;
+    }
+    $name =~ s,\.,/,g;
+
     if ( ! defined($package_path->{$restricted})) {
 	die "Wrong or unknown restriction '$restricted'\n";
     }
@@ -50,7 +80,7 @@ for my $rule (@rules) {
 	    my $src_fn="$src_dir/$fn_icon";
 	    my $dst_fn=$package_path->{$restricted}."$dst_path/".$fn_icon;
 	    if ( -s $src_fn) {
-		#print STDERR "$fn_icon	---> $dst_fn\n";
+		print STDERR "$fn_icon	---> $dst_fn\n" if $VERBOSE>2 || $DEBUG;
 		my $dir = dirname($dst_fn);
 		mkpath $dir  || warn "Cannot create $dir:$!\n";
 		copy($src_fn,$dst_fn)  || warn "Cannot copy $src_fn,$dst_fn:$!\n";;
@@ -69,3 +99,34 @@ if ( $write_output) {
     $fo->close();
 }
 
+
+__END__
+
+=head1 NAME
+
+B<copy_icons_to_debian_package_dirs.pl> Version 0.1
+
+=head1 DESCRIPTION
+
+B<copy_icons_to_debian_package_dirs.pl> is a program to copy 
+This little helper copies according to the icons.xml File
+and the geoinfo->restrictions tag
+all icons will be placed into there package directory
+the default src_dir if build/*
+
+
+=head1 SYNOPSIS
+
+B<Common usages:>
+
+geoinfo.pl [-d] [--man]
+
+=item B<--man>
+
+Print this small usage
+
+=item B<-d>
+
+Add some more Debug Output
+
+=back
