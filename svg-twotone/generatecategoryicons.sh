68eddb1a2a5fd6ea527bb0dfb01f94f334c6a8dd
twain <twain@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-09-11 01:57:38 +0000

    set icon generation defaults
    git-svn-id: http://svn.openstreetmap.org/applications/share/map-icons@10613 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- svg-twotone/generatecategoryicons.sh
+++ svg-twotone/generatecategoryicons.sh
@@ -4,7 +4,6 @@ TYPES=(             'accommodation' 'amenity' 'education' 'food'    'health'  'l
 FORGROUND_COLOURS=( '#0092DA'       '#734A08' '#ffffff'   '#0092DA' '#ffffff' '#999999' '#ffffff' '#000000' '#000000'  '#ffffff'  '#39AC39' '#ffffff' '#ffffff' )
 BACKGROUND_COLOURS=('none'          'none'    '#39AC39'   'none'    '#DA0092' 'none'    '#000000' 'none'     'none'    '#AC39AC'  'none'    '#734A08' '#0092DA')
 SIZES=(32 24 20)
-SIZES=(32)
 
 for (( i = 0 ; i < ${#TYPES[@]} ; i++ )) do
 
@@ -17,7 +16,7 @@ for (( i = 0 ; i < ${#TYPES[@]} ; i++ )) do
     for (( j = 0 ; j < ${#SIZES[@]} ; j++ )) do
       ./recolourtopng.sh ${FILE} ${BACKGROUND_COLOURS[i]} ${BACKGROUND_COLOURS[i]} ${FORGROUND_COLOURS[i]} ${SIZES[j]} ${FOLDERNAME}/${FILE%.*}.${SIZES[j]}
     done
-#    ./recolour.sh ${FILE} ${BACKGROUND_COLOURS[i]} ${BACKGROUND_COLOURS[i]} ${FORGROUND_COLOURS[i]} > ${FOLDERNAME}/${FILE%.*}.svg
+    ./recolour.sh ${FILE} ${BACKGROUND_COLOURS[i]} ${BACKGROUND_COLOURS[i]} ${FORGROUND_COLOURS[i]} > ${FOLDERNAME}/${FILE%.*}.svg
   done
 
 done
